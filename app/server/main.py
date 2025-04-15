1febed011f721bd78c48adc43ca647123604a607
amirhnajafiz <najafizadeh21@gmail.com>
2025-04-15 15:35:53 -0400

    add: flask app configuration

--- app/server/main.py
+++ app/server/main.py
@@ -6,14 +6,17 @@ from src import router
 
 # create a Flask app
 app = Flask(__name__, static_folder='public/static', template_folder='public')
-app.config["JSON_SORT_KEYS"] = False
 
-# set CORS middleware for the app
-CORS(app)
+# Flask app configurations
+app.config["JSON_SORT_KEYS"] = False
+app.config["DEBUG"] = True
+
+# set CORS middleware for the app with specific origins
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # configure the routes
 router.configure_routes(app)
 
 # run the app
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)  # set the port number here
+    app.run(debug=app.config["DEBUG"], port=5000)  # set the port number here
