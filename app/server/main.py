bcaf1ec30a83aba51da08393bd45e8746f5cb21c
amirhnajafiz <najafizadeh21@gmail.com>
2025-04-15 15:26:05 -0400

    init: python flask

---
+++ app/server/main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from src import router
+
+
+
+# create a Flask app
+app = Flask(__name__, static_folder='public/static', template_folder='public')
+app.config["JSON_SORT_KEYS"] = False
+
+# configure the routes
+router.configure_routes(app)
+
+# run the app
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)  # set the port number here
---
+++ app/server/src/router.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+
+
+def configure_routes(app: Flask):
+    """
+    Configure the routes for the Flask app.
+
+    Args:
+        app (Flask): The Flask app instance.
+    """
