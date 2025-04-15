899085f8fe641f29f26af2aa1696dbdab248f99e
amirhnajafiz <najafizadeh21@gmail.com>
2025-04-15 12:46:21 -0400

    update: linguistics logic to use spacy

--- scripts/pipeline/linguistics.py
+++ scripts/pipeline/linguistics.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from difflib import get_close_matches
-import random
+import spacy
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 
@@ -10,13 +10,14 @@ def get_genre(df):
     """
     return df['genre'].unique()
 
-def convert_genres(path: str, genre_list: list):
+def convert_genres(path: str, nlp: any, genre_list: any):
     """
     Convert the genres in the dataset to the closest existing value in the genre list.
 
     Args:
         path (str): The path to the dataset.
-        genre_list (list): The list of existing genres.
+        nlp (any): The spaCy model used for vectorization.
+        genre_list (any): The list of existing genres.
     """
     # load the dataset
     df = pd.read_csv(path)
@@ -25,9 +26,11 @@ def convert_genres(path: str, genre_list: list):
     def find_closest_genre(genre):
         if pd.isna(genre):  # check if the genre is NaN
             return genre  # keep NaN as is
-        matches = get_close_matches(str(genre), genre_list, n=1, cutoff=0.6)
-        return matches[0] if matches else random.choice(genre_list)  # use the closest match or a random genre
-        return matches[0] if matches else genre  # use the closest match or keep the original genre
+        other_vec = nlp(genre).vector
+        similarities = cosine_similarity([other_vec], main_vectors)[0]
+        best_match_index = similarities.argmax()
+        best_match = genre_list[best_match_index]
+        return best_match
 
     # replace the genres in the dataset with the closest existing value
     df['genre'] = df['genre'].apply(find_closest_genre)
@@ -40,8 +43,14 @@ if __name__ == "__main__":
     # load the base dataset
     df = pd.read_csv('tmp/mxmh_survey_results.csv')
 
-    # get the genre of the survey results
-    genre = get_genre(df)
+    # load the spacy model
+    nlp = spacy.load("en_core_web_md")
+
+    # get the genres of the survey results
+    genres = get_genre(df)
+
+    # convert genres to spaCy vectors
+    main_vectors = [nlp(genre).vector for genre in genres]
 
     # list of existing sources
     datasets = ['tmp/apple_music_dataset.csv', 'tmp/spotify_2000_tops.csv', 'tmp/spotify_music_dataset.csv', 'tmp/spotify_song_attributes.csv']
@@ -50,4 +59,4 @@ if __name__ == "__main__":
         print(f"Converting genres in {dataset} to the closest existing value in the genre list...")
 
         # convert the genres in the dataset to the closest existing value in the genre list
-        convert_genres(dataset, genre)
+        convert_genres(dataset, nlp, main_vectors)
