1d5e060c548e7afc9e69b5d014676459918bb7bb
amirhnajafiz <najafizadeh21@gmail.com>
2025-04-19 19:09:46 -0400

    init: home page sections

--- app/dashboard/src/pages/Home.jsx
+++ app/dashboard/src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import './Home.css'
+import "./Home.css";
 
 function Home() {
   return (
-    <>
-      <div>Home</div>
-    </>
-  )
+    <div className="grid-container">
+      <div className="section section-1">Section 1</div>
+      <div className="section section-2">Section 2</div>
+      <div className="section section-3">Section 3</div>
+      <div className="section section-4">Section 4</div>
+      <div className="section section-5">Section 5</div>
+      <div className="section section-6">Section 6</div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
