9f3761e507aa1c80e84fc2d02bd8e35e47f6902d
amirhnajafiz <najafizadeh21@gmail.com>
2025-04-19 13:59:23 -0400

    add: pagination for api

--- app/server/src/api.py
+++ app/server/src/api.py
@@ -7,10 +7,14 @@ from src import config
 
 def get_data_by_columns():
     """
-    Get data from the CSV file based on the specified columns.
+    Get data from the CSV file based on the specified columns with pagination.
     """
     # get the columns from the request
     columns = request.args.getlist('columns')
+    
+    # get pagination parameters
+    page = int(request.args.get('page', 1))  # default to page 1
+    per_page = 100  # fixed items per page
     
     # read the CSV file
     df = pd.read_csv(config.DATASET_PATH)
@@ -21,4 +25,14 @@ def get_data_by_columns():
     else:
         filtered_df = df
     
-    return jsonify(filtered_df.to_dict(orient='records'))
+    # apply pagination
+    start = (page - 1) * per_page
+    end = start + per_page
+    paginated_df = filtered_df.iloc[start:end]
+    
+    return jsonify({
+        "records": paginated_df.to_dict(orient='records'),
+        "page": page,
+        "per_page": per_page,
+        "total_pages": (len(filtered_df) + per_page - 1) // per_page,
+    }), 200
