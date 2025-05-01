123ef556a278224cb16637051be8d1662c3a9a60
IliyaMi <iliya.mi78@gmail.com>
2025-05-01 17:06:27 -0400

    Update KNN script

--- scripts/customize/knn.py
+++ scripts/customize/knn.py
@@ -25,7 +25,7 @@ knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(spotify_data[common_features], spotify_data['country'])
 
 # predict the country for the mental health dataset
-mental_health_data['predicted_country'] = knn.predict(mental_health_data[common_features])
+mental_health_data['country'] = knn.predict(mental_health_data[common_features])
 
 # save the updated dataset
 mental_health_data.to_csv('tmp/merged_attributes_and_mental_health_with_country.csv', index=False)
