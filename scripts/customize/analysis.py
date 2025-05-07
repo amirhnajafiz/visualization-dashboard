62bf494d3d88c24a82cf1e325b6cc8932c437599
amirhnajafiz <najafizadeh21@gmail.com>
2025-05-07 11:42:35 -0400

    feat: new dataset

--- scripts/customize/analysis.py
+++ scripts/customize/analysis.py
@@ -13,6 +13,10 @@ print(f"Number of rows: {df.shape[0]}")
 print("Number of unique values in each column:")
 print(df.nunique())
 
+# print number of items per each country
+print("Number of items per each country:")
+print(df['country'].value_counts())
+
 # print the top 5 rows of the dataset
 print("Top 5 rows of the dataset:")
 print(df.head())
